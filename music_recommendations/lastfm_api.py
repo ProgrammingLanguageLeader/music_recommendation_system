88529ffc16d22fd6eb0599ff515d7b5197e50de3
Dmitry <vip.shoroch@gmail.com>
2018-05-29 03:25:03 +0300

    first working version

--- music_recommendations/lastfm_api.py
+++ music_recommendations/lastfm_api.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 import pylast
+import math
 
 from config import Config
 
@@ -10,46 +12,82 @@ class LastfmNetwork:
             api_key=Config.API_KEY
         )
 
-    def fetch_similar_artists(self, artist_name, count=10):
-        artist_obj = self.network.get_artist(artist_name)
+    def fetch_similar_artists(self, artists_names, limit=10):
+        artists = [
+            self.network.get_artist(artist_name)
+            for artist_name in artists_names
+        ]
+        similar_artists = []
 
-        similar_artists = artist_obj.get_similar(limit=count)
-        if similar_artists:
-            similar_artists = [
-                str(artist.item)
-                for artist in similar_artists
+        for artist in artists:
+            similar_to_single_artist = artist.get_similar(limit=limit)
+            if similar_to_single_artist:
+                similar_artists += [
+                    artist.item
+                    for artist in similar_to_single_artist
+                ]
+                continue
+            top_tags = [
+                tag.item
+                for tag in artist.get_top_tags(limit=5)
             ]
-            return similar_artists
-
-        top_tags = [
-            tag.item
-            for tag in artist_obj.get_top_tags(limit=3)
-        ]
-        top_artists = []
-        for tag_obj in top_tags:
-            top_artists += tag_obj.get_top_artists(limit=count // 3)
-
-        similar_artists = [
-            str(artist_obj.item)
-            for artist_obj in top_artists
-        ]
+            similar_to_single_artist = []
+            tag_limit = math.ceil(limit / 5)
+            for tag in top_tags:
+                similar_to_single_artist += tag.get_top_artists(
+                    limit=tag_limit
+                )
+            similar_artists += [
+                artist.item
+                for artist in similar_to_single_artist
+            ]
         return similar_artists
 
-    def fetch_top_tracks_by_artist(self, artist_name, count=3):
+    @staticmethod
+    def sort_artists_by_tag_collision(artists):
+        artist_tags = defaultdict(list)
+        for artist in artists:
+            tags = artist.get_top_tags(limit=5)
+            for tag in tags:
+                artist_tags[tag].append(artist)
+        sorted_artists = []
+        artist_tags = sorted(
+            artist_tags.items(),
+            key=lambda key_value: len(key_value[1]),
+            reverse=True
+        )
+        for tag, artists in artist_tags:
+            for artist in artists:
+                if artist not in sorted_artists:
+                    sorted_artists.append(artist)
+        return sorted_artists
+
+    def fetch_top_tracks_by_artist_name(self, artist_name, count=3):
         artist = self.network.get_artist(artist_name)
         tracks = artist.get_top_tracks(limit=count)
         return [str(track.item) for track in tracks]
 
-    def fetch_top_tags_by_artist(self, artist_name, count=3):
-        artist = self.network.get_artist(artist_name)
-        tags = artist.get_top_tags(limit=count)
-        return [str(tag.item) for tag in tags]
+    def fetch_top_artists_by_tag(self, tag_name, limit=10):
+        tag = self.network.get_tag(tag_name)
+        top_artists = tag.get_top_artists(limit=limit)
+        return [
+            artist.item
+            for artist in top_artists
+        ]
 
+    def fetch_top_artists_by_track(self, track_name, limit=10):
+        track_artist, track_title = track_name.split('-')
+        track_artist = track_artist.strip()
+        track_title = track_title.strip()
+        track = self.network.get_track(track_artist, track_title)
+        top_tags = track.get_top_tags()[:5]
+        if not top_tags:
+            top_tags = track.get_artist().get_top_tags()[:5]
+        top_artists = []
+        for tag in top_tags:
+            top_artists += self.fetch_top_artists_by_tag(
+                tag_name=tag.item,
+                limit=limit
+            )
+        return top_artists
 
-if __name__ == '__main__':
-    a = LastfmNetwork()
-    # s = a.get_similar_artists('System of a down')
-    # print(s)
-    s = a.fetch_similar_artists('Lil Peep')
-    print(s)
-    print(a.fetch_top_tracks_by_artist('Самое большое простое число'))
--- music_recommendations/views.py
+++ music_recommendations/views.py
@@ -9,34 +9,60 @@ def index(request):
 
 
 def results(request):
-    artist_names = []
-    tag_names = []
-    track_names = []
+    artists_limit = 20
+    tracks_limit = 60
+
+    try:
+        recommend_by = request.GET['recommend_by']
+    except KeyError:
+        recommend_by = ''
+    artists_names = []
+    tags_names = []
+    tracks_names = []
     for key, value in request.GET.items():
-        if re.match(r'artist_name_\d+', key):
-            artist_names.append(value)
-        if re.match(r'tag_name_\d+', key):
-            tag_names.append(value)
-        if re.match(r'track_name_\d+', key):
-            track_names.append(value)
+        if re.match(r'artist_name_\d+', key) and value:
+            artists_names.append(value)
+        if re.match(r'tag_name_\d+', key) and value:
+            tags_names.append(value)
+        if re.match(r'track_name_\d+', key) and value:
+            tracks_names.append(value)
 
     network = LastfmNetwork()
     recommended_artists = []
-    if artist_names:
-        for artist in artist_names:
-            recommended_artists += network.fetch_similar_artists(artist)
+    recommended_tracks = []
+    if recommend_by == 'artists':
+        similar_artists = network.fetch_similar_artists(
+            artists_names=artists_names,
+            limit=10
+        )
+        recommended_artists = network.sort_artists_by_tag_collision(
+            similar_artists
+        )
+    elif recommend_by == 'tracks':
+        for track_name in tracks_names:
+            recommended_artists += network.fetch_top_artists_by_track(
+                track_name
+            )
+        recommended_artists = network.sort_artists_by_tag_collision(
+            recommended_artists
+        )
+    elif recommend_by == 'tags':
+        for tag_name in tags_names:
+            recommended_artists += network.fetch_top_artists_by_tag(tag_name)
+        recommended_artists = network.sort_artists_by_tag_collision(
+            recommended_artists
+        )
 
-    # artists_tags = [
-    #     network.fetch_top_tags_by_artist(artist_name)
-    #     for artist_name in recommended_artists
-    # ]
-    #
-    recommended_tracks = ['']
+    for artist in recommended_artists:
+        recommended_tracks += network.fetch_top_tracks_by_artist_name(
+            artist_name=artist
+        )
+
     return render(
         request,
         'results.html',
         context={
-            'recommended_artists': recommended_artists,
-            'recommended_tracks': recommended_tracks,
+            'recommended_artists': recommended_artists[:artists_limit],
+            'recommended_tracks': recommended_tracks[:tracks_limit],
         }
     )
